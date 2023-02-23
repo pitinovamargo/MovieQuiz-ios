084c1c610a23b34f829e0a8d9c966db3e681b984
Margarita Pitinova <pitinovamargo@gmail.com>
2023-02-23 22:14:25 +0100

    Разделение ответственности, создание фабрики, протоколов и делегатов

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController {
+final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
     // MARK: - Lifecycle
 
     @IBOutlet private weak var imageView: UIImageView!
@@ -9,55 +9,48 @@ final class MovieQuizViewController: UIViewController {
     private var currentQuestionIndex: Int = 0
     private var correctAnswers: Int = 0
     
-    private let questions: [QuizQuestion] = [QuizQuestion(image: "The Godfather",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: true),
-                                             QuizQuestion(image: "The Dark Knight",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: true),
-                                             QuizQuestion(image: "Kill Bill",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: true),
-                                             QuizQuestion(image: "The Avengers",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: true),
-                                             QuizQuestion(image: "Deadpool",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: true),
-                                             QuizQuestion(image: "The Green Knight",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: true),
-                                             QuizQuestion(image: "Old",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: false),
-                                             QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: false),
-                                             QuizQuestion(image: "Tesla",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: false),
-                                             QuizQuestion(image: "Vivarium",
-                                                          text: "Рейтинг этого фильма больше чем 6?",
-                                                          correctAnswer: false),
-    ]
-    
+    private let questionsAmount: Int = 10
+    private var questionFactory: QuestionFactoryProtocol?
+    private var currentQuestion: QuizQuestion?
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        let first: QuizQuestion = questions[currentQuestionIndex]
-        let firstQuestionModel: QuizStepViewModel = convert(model: first)
-        show(quiz: firstQuestionModel)
+        questionFactory = QuestionFactory(delegate: self)
+        
+        questionFactory?.requestNextQuestion()
+    }
+    // MARK: - QuestionFactoryDelegate
+
+    func didReceiveNextQuestion(question: QuizQuestion?) {
+        guard let question = question else {
+              return
+          }
+          
+          currentQuestion = question
+          let viewModel = convert(model: question)
+        DispatchQueue.main.async { [weak self] in
+                self?.show(quiz: viewModel)
+            }
     }
     
+    // MARK: - Actions
+    
     @IBAction private func yesButtonClicked(_ sender: Any) {
-        let currentQuestion: QuizQuestion = questions[currentQuestionIndex]
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
         let givenAnswer = true
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     @IBAction private func noButtonClicked(_ sender: Any) {
-        let currentQuestion = questions[currentQuestionIndex]
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
         let givenAnswer = false
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
+    
+    // MARK: - Private functions
     
     private func showAnswerResult(isCorrect: Bool) {
         
@@ -71,16 +64,17 @@ final class MovieQuizViewController: UIViewController {
             imageView.layer.borderColor = UIColor.ypRed.cgColor
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.showNextQuestionOrResults()
         }
     }
     
     private func showNextQuestionOrResults() {
-      if currentQuestionIndex == questions.count - 1 {
+      if currentQuestionIndex == questionsAmount - 1 {
           let resultAlert = QuizResultsViewModel(
                                 title: "Этот раунд окончен",
-                                text: "Ваш результат: \(correctAnswers)/\(questions.count)",
+                                text: "Ваш результат: \(correctAnswers)/\(questionsAmount)",
                                 buttonText: "Сыграть еще раз")
           
           show(quiz: resultAlert)
@@ -88,9 +82,7 @@ final class MovieQuizViewController: UIViewController {
           currentQuestionIndex += 1
           imageView.layer.borderWidth = 0
 
-          let nextQuestion = questions[currentQuestionIndex]
-          let nextQuestionModel = convert(model: nextQuestion)
-          show(quiz: nextQuestionModel)
+          questionFactory?.requestNextQuestion()
       }
     }
     
@@ -106,13 +98,13 @@ final class MovieQuizViewController: UIViewController {
             message: result.text,
             preferredStyle: .alert)
         
-        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+        let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+            guard let self = self else { return }
+            
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
-            let firstQuestion = self.questions[self.currentQuestionIndex]
-            let viewModel = self.convert(model: firstQuestion)
-            self.show(quiz: viewModel)
+            self.questionFactory?.requestNextQuestion()
         }
         
         alert.addAction(action)
@@ -124,26 +116,7 @@ final class MovieQuizViewController: UIViewController {
         return QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
             question: model.text,
-            questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
     }
     
 }
-
-private struct QuizQuestion {
-  let image: String
-  let text: String
-  let correctAnswer: Bool
-}
-
-private struct QuizStepViewModel {
-  let image: UIImage
-  let question: String
-  let questionNumber: String
-}
-
-private struct QuizResultsViewModel {
-  let title: String
-  let text: String
-  let buttonText: String
-}
-
---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Margarita Pitinova on 23.02.2023.
+//
+
+import Foundation
+
+protocol QuestionFactoryDelegate: AnyObject {
+    func didReceiveNextQuestion(question: QuizQuestion?)  
+}
